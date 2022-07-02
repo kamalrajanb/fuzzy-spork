16b4b7c34579f7a514833c82e55b3d2554e50115
Kamal Rajan B <108523973+kamalrajanb@users.noreply.github.com>
2022-07-02 14:18:55 +0530

    Update mulest.py

--- mulest.py
+++ mulest.py
@@ -11,12 +11,12 @@ cursor.execute("INSERT INTO MOVIES VALUES('Maaran','Dhanush','Malavika Mohanan',
 cursor.execute("INSERT INTO MOVIES VALUES('Master','Vijay','Malavika Mohanan','Lokesh Kanagaraj',2021)")
 cursor.execute("INSERT INTO MOVIES VALUES('Petta','Rajinikanth','Simran & Trisha','Karthik Subbaraj',2019)")
 cursor.execute("INSERT INTO MOVIES VALUES('Bigil','Vijay','Nayanthara','Atlee',2019)")
-print("\t\t\t\tMOVIE DETAILS DATABASE")
+print("\n\t\t\t\tMOVIE DETAILS DATABASE\n")
 allData = cursor.execute("SELECT * FROM MOVIES").fetchall()
 for i in allData:
     title, actor, actress, director, releasedYear = i
     print("{t:<20}{a:<25}{ats:<25}{d:<25}{yr:<5}".format(t=title, a=actor, ats=actress, d=director, yr=releasedYear))
-print("\n\n\t\t\t\tACTOR QUERY")
+print("\n\n\t\t\t\tACTOR QUERY\n")
 actorQuery = cursor.execute("SELECT * FROM MOVIES WHERE ACTOR = 'Vijay'").fetchall()
 for i in actorQuery:
     title, actor, actress, director, releasedYear = i
